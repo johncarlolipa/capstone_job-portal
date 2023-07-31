98814fab444e9cc8924230df78f7671922f6c819
benmeisterz <ben.naldoza@gmail.com>
2023-07-31 14:21:23 +0800

    added route ben

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const path = require("path");
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-app.use(express.static("public"));
-app.use(expressLayouts);
-app.set("layout", "./layouts/master");
-app.set("view engine", "ejs");
+app.use(express.static('public'))
+app.use(expressLayouts)
+app.set('layout', './layouts/master')
+app.set('view engine', 'ejs')
 
-app.get("/", function (req, res) {
-  res.render("pages/index");
-});
+app.get('/', function (req, res) {
+  res.render('pages/index')
+})
 
-app.get("/about", function (req, res) {
-  res.render("pages/about", {
+app.get('/ben', function (req, res) {
+  res.send('ben')
+})
+
+app.get('/about', function (req, res) {
+  res.render('pages/about', {
     people: [
       {
-        name: "Lee",
+        name: 'Lee',
       },
       {
-        name: "John",
+        name: 'John',
       },
     ],
-  });
-});
+  })
+})
 
-app.get("/login", function (req, res) {
-  res.render("pages/login", {
-    layout: "./layouts/authentication",
-  });
-});
+app.get('/login', function (req, res) {
+  res.render('pages/login', {
+    layout: './layouts/authentication',
+  })
+})
 
-app.listen(3000);
-console.log("Server is running");
+app.listen(3000)
+console.log('Server is running')
