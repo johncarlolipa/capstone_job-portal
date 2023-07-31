6f9a441c71d173515d4e8d5d745cdc64708a408e
Rovic Villaralvo <102729133+kazewanderboy@users.noreply.github.com>
2023-07-31 13:24:22 +0800

    HAHAHA

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(expressLayouts);
+app.set("layout", "./layouts/master");
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
+app.get("/about", function (req, res) {
+  res.render("pages/about", {
+    people: [
+      {
+        name: "Lee",
+      },
+      {
+        name: "John",
+      },
+    ],
+  });
+});
+
+app.get("/login", function (req, res) {
+  res.render("pages/login", {
+    layout: "./layouts/authentication",
+  });
+});
+
+app.listen(3000);
+console.log("Server is running");
